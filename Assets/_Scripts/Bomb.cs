cc71ee86f9a12a73a998b0d9c5d362579e73917e
Bartosz Gaj <bartosz-gaj@wp.pl>
2017-05-21 22:26:45 +0200

    All filles

---
+++ Assets/_Scripts/Bomb.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+
+    public float _Time = 3.0f;
+    public float _Radius = 5.0f;
+    public float _Power = 3.0f;
+    public GameObject _FX;
+
+	public void DoExplode () {
+        Invoke("Explode", _Time);
+        
+	}
+	
+	
+
+    void Explode()
+    {
+        var colliders = Physics2D.OverlapCircleAll(transform.position, _Radius);
+   
+        foreach(var colider in colliders)
+        {
+            Rigidbody2D rgbody = colider.GetComponent<Rigidbody2D>();
+            if (rgbody == null) continue;
+
+            Vector2 vector = colider.transform.position - transform.position;
+
+            Vector2 dir = vector.normalized;
+            float len = vector.magnitude;
+
+            rgbody.AddForce(dir * _Power * Mathf.Pow(1 / (len + 1), 2)); 
+        }
+        _FX.SetActive(true);
+        Destroy(gameObject, 0.3f);
+    }
+}
---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject _BombPrefab;
+    public int _BombCount = 3;
+    private List<Bomb> _Bombs = new List<Bomb>();
+    public GameObject _ScoreStick;
+    public float _ScoreTime = 6.0f;
+    public GameObject _Floor;
+
+    public Text _ScoreText;
+
+
+	
+	// Update is called once per frame
+	void Update () {
+        bool lmp = Input.GetMouseButtonUp(0);
+
+        if(lmp && _BombCount > 0)
+        {
+            Vector2 pos = Input.mousePosition;
+            Vector2 worldPosition = Camera.main.ScreenToWorldPoint(pos);
+
+            var go = Instantiate(_BombPrefab, worldPosition, Quaternion.identity);
+
+            var bombScript = go.GetComponent<Bomb>();
+                _Bombs.Add(bombScript);
+
+            _BombCount--;
+            if(_BombCount == 0)
+            {
+                foreach(var bomb in _Bombs)
+                {
+                    bomb.DoExplode();
+                }
+                Invoke("CalculateScore", _ScoreTime);
+
+
+
+            }
+        }
+        if(_ScoreStick.activeInHierarchy)
+        {
+            var height = _ScoreStick.transform.position.y - _Floor.transform.position.y;
+            var score = 1000.0f / (height + 0.1f);
+            _ScoreText.text = "SCORE: " + Mathf.RoundToInt(score);
+        }
+
+    }
+
+    void CalculateScore()
+    {
+        _ScoreStick.SetActive(true);
+    }
+}
